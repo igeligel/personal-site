c1c7ff2ee5c12403a661a4239211bfa88169c00c
igeligel <kevinigeligeligel@gmail.com>
2017-10-19 21:27:51 +0200

    refactor(icons): general-icons
    refactor inline attributes to scoped css

--- components/general-icons/Email.vue
+++ components/general-icons/Email.vue
@@ -1,15 +1,5 @@
 <template>
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="#b0b5b3"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-  >
+  <svg viewBox="0 0 24 24">
     <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
     <polyline points="22,6 12,13 2,6" />
   </svg>
@@ -26,5 +16,10 @@ svg {
   max-height: 25px;
   max-width: 25px;
   margin: 16px;
+  width: 24px;
+  height: 24px;
+  fill: none;
+  stroke: #b0b5b3;
+  stroke-width: 2px;
 }
 </style>

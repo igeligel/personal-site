e85b17c150f64713567fa11570009bd19d5a1450
igeligel <kevinigeligeligel@gmail.com>
2017-10-22 00:22:22 +0200

    refactor(icons)
    refactor vue showcase icon to fit prettier and extract inline styles

--- components/technology-icons/vuejs.vue
+++ components/technology-icons/vuejs.vue
@@ -1,8 +1,14 @@
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300">
+  <svg viewBox="0 0 300 300">
     <g>
-      <polygon style="fill:#a4a4a4;" points="182.9,26.5 150,83.6 117.1,26.5 7.4,26.5 150,273.5 292.6,26.5 	"/>
-      <polygon style="fill:#474747;" points="182.9,26.5 150,83.6 117.1,26.5 64.5,26.5 150,174.7 235.5,26.5 	"/>
+      <polygon
+        class="letter__outside"
+        points="182.9,26.5 150,83.6 117.1,26.5 7.4,26.5 150,273.5 292.6,26.5"
+      />
+      <polygon
+        class="letter__inside"
+        points="182.9,26.5 150,83.6 117.1,26.5 64.5,26.5 150,174.7 235.5,26.5"
+      />
     </g>
   </svg>
 </template>
@@ -13,9 +19,17 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 svg {
   max-width: 40px;
   max-height: 40px;
+
+  .letter__inside {
+    fill: #474747;
+  }
+
+  .letter__outside {
+    fill: #a4a4a4;
+  }
 }
 </style>

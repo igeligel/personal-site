5c6178950877d1ab052a4b0bd8483f4efdf20670
igeligel <kevinigeligeligel@gmail.com>
2017-10-26 20:01:02 +0200

    refactor(components): refactor close
    prettify template tag and extract inline styles to seperate scss style
    tag

--- components/close.vue
+++ components/close.vue
@@ -1,20 +1,28 @@
-
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#706F6F" stroke="#706F6F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x">
-    <line x1="18" y1="6" x2="6" y2="18"></line>
-    <line x1="6" y1="6" x2="18" y2="18"></line>
+  <svg viewBox="0 0 24 24">
+    <line x1="18" y1="6" x2="6" y2="18" />
+    <line x1="6" y1="6" x2="18" y2="18" />
   </svg>
 </template>
 
 <script>
 export default {
   name: 'CloseSvg',
-}
+};
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+$grey: #706F6F;
+
 svg {
   max-width: 40px;
   max-height: 40px;
+  width: 24px;
+  height: 24px;
+  fill: $grey;
+  stroke: $grey;
+  stroke-width: 2px;
+  stroke-linecap: round;
+  stroke-linejoin: round;
 }
 </style>

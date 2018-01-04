36be972dff3a122649781759d2bbc23ec91b6cf0
igeligel <kevinigeligeligel@gmail.com>
2018-01-04 21:56:40 +0100

    refactor(components): refactor arrow upper right component
    run prettier on file

--- components/ArrowUpperRight.vue
+++ components/ArrowUpperRight.vue
@@ -1,7 +1,18 @@
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-up-right" style="transform:translate(0px, 6px);">
-    <line x1="6" y1="18" x2="18" y2="6"></line>
-    <polyline points="9 6 18 6 18 15"></polyline>
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="feather feather-arrow-up-right"
+    style="transform:translate(0px, 6px);"
+  >
+    <line x1="6" y1="18" x2="18" y2="6" />
+    <polyline points="9 6 18 6 18 15" />
   </svg>
 </template>
 

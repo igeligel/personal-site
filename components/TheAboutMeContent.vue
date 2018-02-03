8f7b62a4cd9db7948c3e30eb69c521fa62efbd8f
igeligel <kevinigeligeligel@gmail.com>
2018-02-03 16:34:48 +0100

    refactor(pages): refactor about me page
    refactor content into own component

---
+++ components/TheAboutMeContent.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="about-me__content">
+    <h1 class="about-me__heading--first">About me</h1>
+    <p class="about-me__paragraph">
+      Hey there, I’m Kevin! I am a web developer living in Berlin. I love
+      crafting stuff in the web and am really deep into <VueInlineSvg /> and
+      .NET
+    </p>
+    <p class="about-me__paragraph">
+      You can find my resume on the following link:
+    </p>
+    <a
+      aria-label="Link to open resume"
+      class="about-me__link"
+      rel="noopener noreferrer"
+      :href="resumeLink"
+      target="_blank"
+    >
+      Resume <ArrowUpperRightSvg />
+    </a>
+    <p class="about-me__paragraph">
+      Also I am a passionate blog writer. You can find my blog articles on this
+      page or on medium.
+    </p>
+    <p class="about-me__paragraph">
+      I have more than 20.000 views on my articles and more than 2000 claps. You
+      can check the articles on medium here:
+    </p>
+    <a
+      aria-label="Link to open medium profile page"
+      class="about-me__link"
+      rel="noopener noreferrer"
+      :href="mediumProfileLink"
+      target="_blank"
+    >
+      Medium <ArrowUpperRightSvg />
+    </a>
+    <h2 class="about-me__heading--second">Technologies I use</h2>
+    <TechnologiesList />
+  </div>
+</template>
+
+<script>
+import ArrowUpperRightSvg from './ArrowUpperRight';
+import VueInlineSvg from './technology-icons/VueInline';
+import TechnologiesList from './TechnologiesList';
+
+export default {
+  name: 'TheAboutMeContent',
+  components: {
+    ArrowUpperRightSvg,
+    VueInlineSvg,
+    TechnologiesList,
+  },
+  props: {
+    resumeLink: String,
+    mediumProfileLink: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/scss/mixins.scss';
+
+$text-grey: #706f6f;
+$text-blue: #4096c4;
+
+.about-me {
+  &__heading {
+    &--first {
+      color: $text-grey;
+      font-size: 1.5625em;
+      font-weight: 300;
+      margin-top: 0.75em;
+      margin-bottom: 0.75em;
+    }
+
+    &--second {
+      color: $text-grey;
+      font-size: 1.3em;
+      font-weight: 300;
+      line-height: 1.5625em;
+      margin-bottom: 0.5em;
+    }
+  }
+
+  &__link {
+    @include no-text-decoration();
+
+    color: $text-blue;
+    font-size: 1.125em;
+    line-height: (7 / 3) + 0em;
+    padding-left: (1 / 3) + 0em;
+  }
+
+  &__content {
+    padding-left: 1.5625em;
+    padding-right: 1.5625em;
+  }
+
+  &__paragraph {
+    color: $text-grey;
+    font-size: 1em;
+    font-weight: 300;
+    line-height: 1.5625em;
+  }
+}
+</style>
--- pages/about/index.vue
+++ pages/about/index.vue
@@ -8,66 +8,27 @@
         <ProfileLinks :socialMediaList="socialMedia" />
       </div>
     </div>
-    <div class="about-me__content">
-      <h1 class="about-me__heading--first">About me</h1>
-      <p class="about-me__paragraph">
-        Hey there, I’m Kevin! I am a web developer living in Berlin. I love
-        crafting stuff in the web and am really deep into <VueInlineSvg /> and
-        .NET
-      </p>
-      <p class="about-me__paragraph">
-        You can find my resume on the following link:
-      </p>
-      <a
-        aria-label="Link to open resume"
-        class="about-me__link"
-        rel="noopener noreferrer"
-        :href="resume"
-        target="_blank"
-      >
-        Resume <ArrowUpperRightSvg />
-      </a>
-      <p class="about-me__paragraph">
-        Also I am a passionate blog writer. You can find my blog articles on
-        this page or on medium.
-      </p>
-      <p class="about-me__paragraph">
-        I have more than 20.000 views on my articles and more than 2000 claps.
-        You can check the articles on medium here:
-      </p>
-      <a
-        aria-label="Link to open medium profile page"
-        class="about-me__link"
-        rel="noopener noreferrer"
-        :href="mediumProfile"
-        target="_blank"
-      >
-        Medium <ArrowUpperRightSvg />
-      </a>
-      <h2 class="about-me__heading--second">Technologies I use</h2>
-      <TechnologiesList />
-    </div>
+    <TheAboutMeContent
+      :resumeLink="resumeLink"
+      :mediumProfileLink="mediumProfileLink"
+    />
   </div>
 </template>
 
 <script>
-import ArrowUpperRightSvg from '~/components/ArrowUpperRight';
-import VueInlineSvg from '~/components/technology-icons/VueInline';
-
-import ProfileLinks from '~/components/ProfileLinks';
-import TechnologiesList from '~/components/TechnologiesList';
 import ProfilePicture from '~/components/ProfilePicture';
+import ProfileLinks from '~/components/ProfileLinks';
+import TheAboutMeContent from '~/components/TheAboutMeContent';
+
 import AboutHead from './AboutHead';
 import ProfilePictureRaw from '~/assets/img/profile-border-radius.png';
 
 export default {
   head: AboutHead,
   components: {
-    ArrowUpperRightSvg,
-    VueInlineSvg,
-    ProfileLinks,
-    TechnologiesList,
     ProfilePicture,
+    ProfileLinks,
+    TheAboutMeContent,
   },
   data() {
     return {
@@ -104,9 +65,9 @@ export default {
           url: 'https://github.com/igeligel',
         },
       ],
-      resume:
+      resumeLink:
         'https://drive.google.com/file/d/0B5bsFD4wxCdmczVJQnFLWXAwNEU/view?usp=sharing',
-      mediumProfile: 'https://medium.com/@kevin_peters',
+      mediumProfileLink: 'https://medium.com/@kevin_peters',
       profilePicture: ProfilePictureRaw,
     };
   },
@@ -132,57 +93,11 @@ export default {
 </script>
 
 <style scoped lang="scss">
-@import '../../assets/scss/mixins.scss';
-
-$text-grey: #706f6f;
-$text-blue: #4096c4;
-
-.about-me {
-  &__container {
-    font-family: 'Lato', sans-serif;
-    max-width: 1000px;
-    margin: auto;
-    padding-bottom: 65px;
-    text-rendering: optimizeLegibility;
-  }
-
-  &__heading {
-    &--first {
-      color: $text-grey;
-      font-size: 1.5625em;
-      font-weight: 300;
-      margin-top: 0.75em;
-      margin-bottom: 0.75em;
-    }
-
-    &--second {
-      color: $text-grey;
-      font-size: 1.3em;
-      font-weight: 300;
-      line-height: 1.5625em;
-      margin-bottom: 0.5em;
-    }
-  }
-
-  &__link {
-    @include no-text-decoration();
-
-    color: $text-blue;
-    font-size: 1.125em;
-    line-height: (7 / 3) + 0em;
-    padding-left: (1 / 3) + 0em;
-  }
-
-  &__content {
-    padding-left: 1.5625em;
-    padding-right: 1.5625em;
-  }
-
-  &__paragraph {
-    color: $text-grey;
-    font-size: 1em;
-    font-weight: 300;
-    line-height: 1.5625em;
-  }
+.about-me__container {
+  font-family: 'Lato', sans-serif;
+  max-width: 1000px;
+  margin: auto;
+  padding-bottom: 65px;
+  text-rendering: optimizeLegibility;
 }
 </style>

038dcfd1cc6e3c3e0afd9b7abf29853805bdbba2
Kevin Peters <kevin.peters@klarna.com>
2019-07-23 09:15:49 +0200

    feat: make about page work

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Helmet from "react-helmet";
 import Layout from "../layout";
-import About from "../components/About/About";
 import config from "../../data/SiteConfig";
 
 class AboutPage extends Component {
@@ -10,7 +9,9 @@ class AboutPage extends Component {
       <Layout>
         <div className="about-container">
           <Helmet title={`About | ${config.siteTitle}`} />
-          <About />
+          <div>
+            LOL
+          </div>
         </div>
       </Layout>
     );

ddacd0401a2f9dd1f4894356bdfc4657ee93340a
Kevin Peters <kevin.peters@klarna.com>
2019-07-24 21:15:49 +0200

    feat: project section and finish blog section

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,6 +7,7 @@ import SEO from "../components/SEO/SEO";
 import config from "../../data/SiteConfig";
 import styled from "styled-components";
 import { Link } from "gatsby";
+import {ProjectList} from '../components/ProjectList'
 
 const NavbarContainer = styled.div`
   flex: 1;
@@ -88,7 +89,7 @@ const UnorderedList = styled.ul`
   margin: 0;
 `;
 
-const ListItemL = styled.a`
+const ListItemL = styled(Link)`
   display: block;
   text-decoration: none;
   width: 100%;
@@ -116,15 +117,15 @@ const ListItemLink = styled.span`
 const StyledLink = styled(Link)`
   // color: palevioletred;
   // font-weight: bold;
-  color:rgba(64,150,196,0.5);
+  color: rgba(64, 150, 196, 0.5);
   font-weight: 300;
   font-family: Lato, sans-serif;
   margin-left: 1em;
   line-height: 2em;
   text-decoration: none;
 
-  &:hover {
-    color:rgba(64,150,196,1);
+  :hover {
+    color: rgba(64, 150, 196, 1);
   }
 `;
 
@@ -139,15 +140,32 @@ const MainStyledLink = styled(Link)`
   text-decoration: none;
 `;
 
-const IconWrapper = styled.div`
-width: 32px;
-height: 32px;
-align-self: center;
-`
+  const IconWrapper = styled.div`
+    width: 32px;
+    height: 32px;
+    align-self: center;
+  `;
+
+const getPostList = postEdges => {
+  const postList = [];
+  postEdges.forEach(postEdge => {
+    postList.push({
+      path: postEdge.node.fields.slug,
+      tags: postEdge.node.frontmatter.tags,
+      cover: postEdge.node.frontmatter.cover,
+      title: postEdge.node.frontmatter.title,
+      date: postEdge.node.fields.date,
+      excerpt: postEdge.node.excerpt,
+      timeToRead: postEdge.node.timeToRead
+    });
+  });
+  return postList;
+};
 
 class Index extends React.Component {
   render() {
     const postEdges = this.props.data.allMarkdownRemark.edges;
+    const postList = getPostList(postEdges);
 
     return (
       <Layout>
@@ -315,18 +333,51 @@ class Index extends React.Component {
           <SectionContainerWrapper>
             <SectionContainer>
               <SectionHeading>Latest Articles</SectionHeading>
-              <PostListing postEdges={postEdges} />
+              <UnorderedList>
+                {postList.map(post => (
+                  <ListItem key={post.title}>
+                    <ListItemL to={post.path}>
+                      <div style={{ display: "flex" }}>
+                        <IconWrapper>
+                          <svg viewBox="0 0 128 128">
+                            <path
+                              d="m-2.3125e-8 8.9337 49.854 0.1586 14.167 24.47 14.432-24.47 49.547-0.1577-63.834 110.14zm126.98 0.6374-24.36 0.0207-38.476 66.052-38.453-66.052-24.749-0.0194 63.211 107.89zm-25.149-0.008-22.745 0.16758l-15.053 24.647-14.817-24.647-22.794-0.1679 37.731 64.476zM25.997 9.3929l23.002 0.0087M25.997 9.3929l23.002 0.0087"
+                              fill="none"
+                            ></path>
+                            <path
+                              d="m25.997 9.3929 23.002 0.0087l15.036 24.958 14.983-24.956 22.982-0.0057-37.85 65.655z"
+                              fill="#35495e"
+                            ></path>
+                            <path
+                              d="m0.91068 9.5686 25.066-0.1711 38.151 65.658 37.852-65.654 25.11 0.0263-62.966 108.06z"
+                              fill="#41b883"
+                            ></path>
+                          </svg>
+                        </IconWrapper>
+                        <ListItemLink>{post.title}</ListItemLink>
+                      </div>
+                    </ListItemL>
+                  </ListItem>
+                ))}
+              </UnorderedList>
             </SectionContainer>
           </SectionContainerWrapper>
-          <div>
-            <h2>Books</h2>
-          </div>
-          <div>
-            <h2>Projects</h2>
-          </div>
-          <div>
-            <h2>Speaking</h2>
-          </div>
+          <SectionContainerWrapper>
+            <SectionContainer>
+              <SectionHeading>Books</SectionHeading>
+            </SectionContainer>
+          </SectionContainerWrapper>
+          <SectionContainerWrapper>
+            <SectionContainer>
+              <SectionHeading>Projects</SectionHeading>
+              <ProjectList />
+            </SectionContainer>
+          </SectionContainerWrapper>
+          <SectionContainerWrapper>
+            <SectionContainer>
+              <SectionHeading>Speaking</SectionHeading>
+            </SectionContainer>
+          </SectionContainerWrapper>
         </div>
       </Layout>
     );

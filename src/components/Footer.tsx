8ee04508512ca01a6950926daecf8d475d53d676
igeligel <kevinigeligeligel@gmail.com>
2021-01-03 00:10:16 +0100

    fix: file

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box, Text } from "@chakra-ui/react";
-import { NavigationLink } from "../../components/NavigationLink.";
+import { NavigationLink } from "../../components/NavigationLink";
 
 const date = new Date();
 const year = date.getFullYear();
